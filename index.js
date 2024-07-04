d1ca44a6cba81463abe8c7ffb123cf9ef9832d9d
Stanley Yu <124069731+Fastestgod@users.noreply.github.com>
2024-07-04 14:42:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-const url = 'https://www.amazon.com/Sony-Noise-Cancelling-Headphones-WH1000XM3/dp/B07G4MNFS1/';
+const url = 'https://www.amazon.com/dp/B0C8PSMPTH/ref=sspa_dk_detail_0?psc=1&pd_rd_i=B0C8PSMPTH&pd_rd_w=1rPOq&content-id=amzn1.sym.386c274b-4bfe-4421-9052-a1a56db557ab&pf_rd_p=386c274b-4bfe-4421-9052-a1a56db557ab&pf_rd_r=GNQXPM489PJR4MYFMHN8&pd_rd_wg=xTp5y&pd_rd_r=8d748c51-438b-4e28-933a-0cdbb7d3e804&s=electronics&sp_csd=d2lkZ2V0TmFtZT1zcF9kZXRhaWxfdGhlbWF0aWM';
 
 async function configureBrowser() {
     const browser = await puppeteer.launch({ headless: true });
@@ -12,22 +12,18 @@ async function configureBrowser() {
 async function checkPrice(page) {
     await page.reload({ waitUntil: 'load', timeout: 0 });
     try {
-        await page.waitForSelector('.a-price.aok-align-center.reinventPricePriceToPayMargin.priceToPay', { timeout: 5000 });
+        await page.waitForSelector('.aok-offscreen', { timeout: 5000 });
         const currentPrice = await page.evaluate(() => {
-            const priceElement = document.querySelector('.a-price.aok-align-center.reinventPricePriceToPayMargin.priceToPay');
+            const priceElement = document.querySelector('.aok-offscreen');
             if (priceElement) {
-                const priceText = priceElement.innerText;
-                return Number(priceText.replace(/[^0-9.-]+/g, ""));
+                const priceText = priceElement.innerText.trim().match(/[$]?[0-9,.]+/)[0];
+                return parseFloat(priceText.replace(/[^0-9.-]+/g, "")).toFixed(2);
             }
             return null;
         });
 
         if (currentPrice !== null) {
-            if (currentPrice < 300) {
-                console.log("BUY!!!! " + currentPrice);
-            } else {
-                console.log("Current price is: " + currentPrice);
-            }
+            console.log(currentPrice);
         } else {
             console.log("Price element not found.");
         }
